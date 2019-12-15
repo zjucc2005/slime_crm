c3e97d64833249cc5709beada2a32b4cebd05cc0
chang.cai <zjucc2005@163.com>
2019-12-15 19:33:54 +0800

    add jquery-ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
 import 'styles/application'
+
 import 'bootstrap'
 import 'jquery'
 import 'js/custom'
