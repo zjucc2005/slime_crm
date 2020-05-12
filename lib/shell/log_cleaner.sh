9fc4e4d5644317a397a90d7bd9b858d46a6d0d08
caichang <chang.cai@quaie.com>
2020-05-12 13:38:34 +0800

    log_cleaner.sh

--- lib/shell/log_cleaner.sh
+++ lib/shell/log_cleaner.sh
@@ -1,8 +1,10 @@
+# 0 0 1 * * /opt/rails-app/slime_crm/lib/shell/log_cleaner.sh >> /opt/rails-app/slime_crm/log/log_cleaner.log 2>&1
+
 echo "============================================="
 echo "`date +"%F %T"` -- running log_cleaner.sh"
 
-log_dir = "/opt/rails-app/slime_crm/log"
-log_backup = "production.log.backup_`date +"%y%m%d"`"
+log_dir="/opt/rails-app/slime_crm/log"
+log_backup="production.log.backup_`date +"%y%m%d"`"
 cd $log_dir
 echo ">> cd $log_dir"
 mv production.log $log_backup
