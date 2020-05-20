60978af6288d17f7f0e69f6150710808aaaf0f39
caichang <chang.cai@quaie.com>
2020-05-20 20:20:41 +0800

    remove index

--- db/migrate/20200520121303_add_index_to_candidate_experiences.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToCandidateExperiences < ActiveRecord::Migration[6.0]
-  def change
-    add_index :candidate_experiences, :org_cn
-    add_index :candidate_experiences, :org_en
-    add_index :candidate_experiences, :title
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_121303) do
+ActiveRecord::Schema.define(version: 2020_05_11_080933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,6 @@ ActiveRecord::Schema.define(version: 2020_05_20_121303) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["candidate_id"], name: "index_candidate_experiences_on_candidate_id"
-    t.index ["org_cn"], name: "index_candidate_experiences_on_org_cn"
-    t.index ["org_en"], name: "index_candidate_experiences_on_org_en"
-    t.index ["title"], name: "index_candidate_experiences_on_title"
   end
 
   create_table "candidate_payment_infos", force: :cascade do |t|
