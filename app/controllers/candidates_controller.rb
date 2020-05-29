2d40ab02ae464d5e557aa33e09a4b22a2a967f53
caichang <chang.cai@quaie.com>
2020-05-29 13:17:05 +0800

    optimized candidates query a little

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -10,9 +10,9 @@ class CandidatesController < ApplicationController
     query = Candidate.expert
     # query code here >>
     query = query.where('candidates.id' => params[:id].strip) if params[:id].present?
-    query = query.where('candidates.name ILIKE :name OR candidates.nickname ILIKE :name', { :name => "%#{params[:name].strip}%" }) if params[:name].present?
-    query = query.where('candidates.phone LIKE :phone OR candidates.phone1 LIKE :phone', { :phone => "%#{params[:phone].strip}%" }) if params[:phone].present?
-    query = query.where('candidates.email ILIKE :email OR candidates.email1 ILIKE :email', { :email => "%#{params[:email].strip}%" }) if params[:email].present?
+    query = query.where('candidates.name ~* :name OR candidates.nickname ~* :name', { :name => params[:name].strip }) if params[:name].present?
+    query = query.where('candidates.phone ~ :phone OR candidates.phone1 ~ :phone', { :phone => params[:phone].strip }) if params[:phone].present?
+    query = query.where('candidates.email ~* :email OR candidates.email1 ~* :email', { :email => params[:email].strip }) if params[:email].present?
     query = query.where('candidates.industry' => params[:industry].strip) if params[:industry].present?
     query = query.where('candidates.is_available' => params[:is_available] == 'nil' ? nil : params[:is_available] ) if params[:is_available].present?
 
@@ -27,7 +27,8 @@ class CandidatesController < ApplicationController
       and_conditions = []
       or_fields = %w[candidates.description candidate_experiences.org_cn candidate_experiences.org_en candidate_experiences.title candidate_experiences.description]
       @terms.each do |term|
-        and_conditions << "(#{or_fields.map{|field| "#{field} ILIKE '%#{term}%'" }.join(' OR ') })"
+        # and_conditions << "(#{or_fields.map{|field| "#{field} ILIKE '%#{term}%'" }.join(' OR ') })"
+        and_conditions << "(#{or_fields.map{|field| "#{field} ~* '#{term}'" }.join(' OR ')})"
       end
       query = query.joins('LEFT JOIN candidate_experiences on candidates.id = candidate_experiences.candidate_id')
       query = query.where(and_conditions.join(' AND '))
